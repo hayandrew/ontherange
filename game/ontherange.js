d9e7c061d179e495c8d71ac6293328be8568d48f
Lu <lujw@ny-lujw-yo.mtvn.ad.viacom.com>
2016-03-17 21:00:55 -0400

    enemies

--- game/ontherange.js
+++ game/ontherange.js
@@ -7,6 +7,10 @@ OTR = {
     width: 0,
     height: 0
   },
+  enemyTimer: 0,
+  enemyCounter: 0,
+  bulletDelay: 0,
+  bulletCounter: 0,
   Container: PIXI.Container,
   TextureCache: PIXI.utils.TextureCache,
   autoDetectRenderer: PIXI.autoDetectRenderer,
@@ -51,37 +55,93 @@ OTR.commonMethods = {
       if (a.z < b.z) return -1;
       if (a.z > b.z) return 1;
       return 0;
+    },
+    hitTestRectangle: function(r1, r2) {
+
+      //Define the variables we'll need to calculate
+      var hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+
+      //hit will determine whether there's a collision
+      hit = false;
+
+      //Find the center points of each sprite
+      r1.centerX = r1.x + r1.width / 2;
+      r1.centerY = r1.y + r1.height / 2;
+      r2.centerX = r2.x + r2.width / 2;
+      r2.centerY = r2.y + r2.height / 2;
+
+      //Find the half-widths and half-heights of each sprite
+      r1.halfWidth = r1.width / 2;
+      r1.halfHeight = r1.height / 2;
+      r2.halfWidth = r2.width / 2;
+      r2.halfHeight = r2.height / 2;
+
+      //Calculate the distance vector between the sprites
+      vx = r1.centerX - r2.centerX;
+      vy = r1.centerY - r2.centerY;
+
+      //Figure out the combined half-widths and half-heights
+      combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+      combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+      //Check for a collision on the x axis
+      if (Math.abs(vx) < combinedHalfWidths) {
+
+        //A collision might be occuring. Check for a collision on the y axis
+        if (Math.abs(vy) < combinedHalfHeights) {
+
+          //There's definitely a collision happening
+          hit = true;
+        } else {
+
+          //There's no collision on the y axis
+          hit = false;
+        }
+      } else {
+
+        //There's no collision on the x axis
+        hit = false;
+      }
+
+      //`hit` will be either `true` or `false`
+      return hit;
     }
   },
   createEnemy: function(){
     var enemy = {
+        "id": ++OTR.enemyCounter,
         "faction": "",
-        "contraint": 400,
+        "constraint": 600,
         "zIndex": 0,
+        "initialX": 0,
+        "turnaround": false,
         "obj": null
       },
       avatar = "",
+      plusOrMinus = Math.random() < 0.5 ? -1 : 1,
       randomValue = Math.floor((Math.random() * 10) + 1);
     if (randomValue >= 5) {
       enemy.faction = "DEMOCRATIC";
-      enemy.contraint += 10 * randomValue;
+      enemy.contraint += 20 * randomValue;
       enemy.avatar = OTR.assets.graphic.urls.actors.player;
     } else {
       enemy.faction = "REPUBLICAN";
-      enemy.contraint += 10 * randomValue;
+      enemy.contraint += 20 * randomValue;
       enemy.avatar = OTR.assets.graphic.urls.actors.player;
     }
+    enemy.contraint += plusOrMinus * 100;
     enemy.obj = new OTR.Sprite(
       OTR.resources[enemy.avatar].texture
     );
-    var posY = Math.floor(Math.random() * ((OTR.stage.height - OTR.props.actors.player.height) - 200 + 1)) + 50;
-    enemy.obj.x = Math.floor(Math.random()) * 1024;
+    var posY = Math.floor(Math.random() * 100) + 100;
+    enemy.obj.width = 330 * 0.5;
+    enemy.obj.height = 500 * 0.5;
+    enemy.obj.x = Math.round(Math.random()) * (OTR.canvasSize.width - enemy.obj.width);
+    enemy.initialX = enemy.obj.x;
     enemy.obj.y = posY;
     enemy.obj.z = posY;
     enemy.obj.vx = 0;
     enemy.obj.vy = 0;
-    enemy.obj.width = 330 * 0.5;
-    enemy.obj.height = 500 * 0.5;
 
     OTR.characters.enemies.push(enemy);
     OTR.stage.addChild(enemy.obj);
@@ -89,26 +149,83 @@ OTR.commonMethods = {
 
   },
 
+  initEnemies: function(){
+    if (OTR.enemyTimer % 240 === 0 && OTR.characters.enemies.length < 6){
+      OTR.commonMethods.createEnemy();
+    }
+  },
+
   update: function(){
+
+    OTR.enemyTimer++;
+    OTR.bulletDelay++;
+    OTR.commonMethods.initEnemies();
 
     OTR.props.actors.player.x += OTR.props.actors.player.vx;
 
     OTR.scene.projectiles.forEach(function(projectile){
+      var bulletHit = false;
+
       projectile.timeToLive -= 1;
       projectile.velocity -= projectile.velocity * 0.02;
       projectile.obj.y -= projectile.velocity;
 
       projectile.obj.width -= projectile.obj.width/80;
       projectile.obj.height -= projectile.obj.height/80;
-      if (projectile.timeToLive <= 0){
-        OTR.scene.projectiles.shift();
+
+      if (projectile.timeToLive <= 50){
+        projectile.active = true;
+      }
+      if (projectile.active){
+        OTR.characters.enemies.forEach(function(enemy){
+          if (OTR.commonMethods.utils.hitTestRectangle(projectile.obj, enemy.obj)){
+            // HIT, remove bullet and enemy
+            console.log("HIT")
+            OTR.characters.enemies = $.grep(OTR.characters.enemies, function(e){
+              return e.id != enemy.id;
+            });
+            OTR.scene.projectiles = $.grep(OTR.scene.projectiles, function(e){
+              return e.id != projectile.id;
+            });
+            OTR.stage.removeChild(enemy.obj);
+            OTR.stage.removeChild(projectile.obj);
+            bulletHit = true;
+          }
+        });
+      }
+
+      if (projectile.timeToLive <= 0 && !bulletHit){
+        OTR.scene.projectiles = $.grep(OTR.scene.projectiles, function(e){
+          return e.id != projectile.id;
+        });
         OTR.stage.removeChild(projectile.obj);
       }
     });
 
     OTR.characters.enemies.forEach(function(enemy){
-      enemy.obj.x += 5;
-
+      if (enemy.initialX > 0 && !enemy.turnaround){
+        enemy.obj.x -= 4;
+      } else if (!enemy.turnaround) {
+        enemy.obj.x += 4;
+      }
+      if (enemy.initialX > 0 && enemy.turnaround){
+        enemy.obj.x += 4;
+      } else if (enemy.turnaround) {
+        enemy.obj.x -= 4;
+      }
+      if (enemy.initialX > 0){
+        if (enemy.obj.x <= enemy.initialX - enemy.constraint) {
+          enemy.turnaround = true;
+        } else if (enemy.obj.x >= enemy.initialX) {
+          enemy.turnaround = false;
+        }
+      } else {
+        if (enemy.obj.x >= enemy.initialX + enemy.constraint) {
+          enemy.turnaround = true;
+        } else if (enemy.obj.x <= enemy.initialX) {
+          enemy.turnaround = false;
+        }
+      }
     });
 
     OTR.renderer.render(OTR.stage);
