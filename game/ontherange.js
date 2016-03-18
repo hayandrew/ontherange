c5f8419ff05bcae141e067c6a78c8b8135e43508
Jwun-Da Lu <jdlu10@gmail.com>
2016-03-18 15:29:41 -0400

    conflicts

--- game/ontherange.js
+++ game/ontherange.js
@@ -203,6 +203,20 @@ OTR.commonMethods = {
       if (a.z > b.z) return 1;
       return 0;
     },
+		getUrlParameter: function(sParam) {
+		    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+		        sURLVariables = sPageURL.split('&'),
+		        sParameterName,
+		        i;
+
+		    for (i = 0; i < sURLVariables.length; i++) {
+		        sParameterName = sURLVariables[i].split('=');
+
+		        if (sParameterName[0] === sParam) {
+		            return sParameterName[1] === undefined ? true : sParameterName[1];
+		        }
+		    }
+		},
       hitEnemy: function(r1, r2) {
 
           //Define the variables we'll need to calculate
